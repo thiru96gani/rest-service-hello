24ee261ae30817106cb2740abacc0f7496d66002
Thirumurugan <thiru.ganesan@silverlakeaxis.com>
2024-04-02 16:07:47 +0800

    First commit by me

---
+++ hello/src/main/java/hello/com/GreetingComponent.java
@@ -0,0 +1,12 @@
+package hello.com;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class GreetingComponent {
+
+	public String getMessage() {
+        return "Hello from GreetingComponent";
+    }
+}
